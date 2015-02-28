251572d60bb6d999bf8d4f3096389aa6b367bca2
Dawn <dawnkuo.hk@gmail.com>
2015-02-28 16:38:10 +0800

    init
    init by as

---
+++ app/src/androidTest/java/com/yobook/YobookApplication.java
@@ -0,0 +1,11 @@
+package com.yobook;
+
+import android.app.Application;
+
+public class YobookApplication extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}
---
+++ app/src/main/java/com/yobook/activity/BaseActivity.java
@@ -0,0 +1,6 @@
+package com.yobook.activity;
+
+import android.app.Activity;
+
+public class BaseActivity extends Activity {
+}
