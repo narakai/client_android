6eb9b9eed04255e8b9008d7ec9a53d114e61433b
Dawn <dawnkuo.hk@gmail.com>
2015-03-07 14:00:27 +0800

    modify YLog

--- app/src/main/java/com/yobook/util/YLog.java
+++ app/src/main/java/com/yobook/util/YLog.java
@@ -5,23 +5,23 @@ import android.util.Log;
 
 public class YLog {
 
-    private static int LEVEL_DEBUG = 0;
+    private static final int LEVEL_DEBUG = 0;
 
-    private static int LEVEL_DEVELOPER = 1;
+    private static final int LEVEL_DEVELOPER = 1;
 
-    private static int LEVEL_TEST = 2;
+    private static final int LEVEL_TEST = 2;
 
-    private static int LEVEL_ALPHA = 3;
+    private static final int LEVEL_ALPHA = 3;
 
-    private static int LEVEL_BETA = 4;
+    private static final int LEVEL_BETA = 4;
 
-    private static int LEVEL_GRAY = 5;
+    private static final int LEVEL_GRAY = 5;
 
-    private static int LEVEL_OFFICIAL = 6;
+    private static final int LEVEL_OFFICIAL = 6;
 
-    private static int LEVEL_OFFICIAL_COLOR = 7;
+    private static final int LEVEL_OFFICIAL_COLOR = 7;
 
-    private static int mLevel = LEVEL_DEBUG;
+    private static int slevel = LEVEL_DEBUG;
 
     public static void i(String tag, String msg) {
         Log.i(tag, msg);
